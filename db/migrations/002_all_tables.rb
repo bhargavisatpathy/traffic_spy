b26b6dd3ce70c49ecd4ac46b98f2d0020283da74
Bhargavi Satpathy <bhargavisatpathy@gmail.com>
2014-12-30 15:27:37 -0700

    edit in migration

--- db/migrations/002_all_tables.rb
+++ db/migrations/002_all_tables.rb
@@ -1,55 +1,5 @@
 Sequel.migration do
   change do
-    create_table(:urls) do
-      primary_key   :id
-      String        :url
-    end
-  end
-
-  change do
-    create_table(:referred_bys) do
-      primary_key   :id
-      String        :referred_by
-    end
-  end
-
-  change do
-    create_table(:request_types) do
-      primary_key   :id
-      String        :request_type
-    end
-  end
-
-  change do
-    create_table(:event_names) do
-      primary_key   :id
-      String        :event_name
-    end
-  end
-
-  change do
-    create_table(:user_agents) do
-      primary_key   :id
-      String        :user_agent
-    end
-  end
-
-  change do
-    create_table(:resolutions) do
-      primary_key   :id
-      String        :width
-      String        :height
-    end
-  end
-
-  change do
-    create_table(:ips) do
-      primary_key   :id
-      String        :ip
-    end
-  end
-
-  change do
     create_table(:payloads) do
       primary_key   :id
       Integer       :url_id
