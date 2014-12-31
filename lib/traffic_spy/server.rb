a0f935e1ffa54bb7b8d2faf433f1f5ed85bb8da3
Alex Robinson <alexandra.scott335@gmail.com>
2014-12-31 14:12:13 -0700

    fixed resolution_id typo

--- lib/traffic_spy/server.rb
+++ lib/traffic_spy/server.rb
@@ -120,8 +120,8 @@ module TrafficSpy
 
     def find_resolution_id(resolution_width, resolution_height)
       if DB.from(:resolutions).select(:width, :height).to_a.any? {|item| item[:width] == resolution_width && item[:height] == resolution_height}
-        request_type_id = DB.from(:resolutions).select(:id).where(:width => resolution_width).where(:height => resolution_height)
-        puts "We found it"
+        resolution_id = DB.from(:resolutions).select(:id).where(:width => resolution_width).where(:height => resolution_height)
+        puts "We found resolution"
       else
         puts "we didn't find it"
         DB.from(:resolutions).insert(:width => resolution_width, :height => resolution_height)
